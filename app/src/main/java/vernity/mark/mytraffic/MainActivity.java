868a863fabff3ef208f0363a9b121da7fcca7dfe
Mark <mark@vernity.com>
2015-12-19 14:46:28 +0700

    Finished_SoundEffect

--- app/src/main/java/vernity/mark/mytraffic/MainActivity.java
+++ app/src/main/java/vernity/mark/mytraffic/MainActivity.java
@@ -1,13 +1,54 @@
 package vernity.mark.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit ประกาศตัวแปร Access Datatype Name
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget to link variable with method
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+
+    } //Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_long);
+                buttonMediaPlayer.start();
+
+            }  //Event
+        });
     }
-}
+
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+        // use findViewById first then the (Button) will appear automatically
+
+
+    }
+
+} //Main Class
